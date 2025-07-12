505fb206df3d72829dd6fbbba2f543fcca63a3a0
Harshdeep Sharma <167959814+Harshdeepsharma2003@users.noreply.github.com>
2025-07-12 08:52:30 -0700

    Delete src directory

--- src/main/java/com/harshdeep/service/ContactsService.java
+++
@@ -1,8 +0,0 @@
-package com.harshdeep.service;
-
-import com.harshdeep.model.Contacts;
-
-public interface ContactsService {
-
-    void insertContact(Contacts contacts) throws Exception;
-}
--- src/main/java/com/harshdeep/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.harshdeep.service;
-
-import com.harshdeep.model.User;
-
-public interface UserService {
-
-    boolean authenticate(String userId, String password) throws Exception;
-    void insertUser(User user) throws Exception;
-}
